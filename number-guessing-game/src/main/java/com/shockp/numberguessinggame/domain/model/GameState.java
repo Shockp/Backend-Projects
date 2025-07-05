5407463fc5f384409dce970059333f98ce6cd94e
Shockp <masque2204@gmail.com>
2025-07-05 14:13:20 +0200

    GameState documented and bug fixed

--- number-guessing-game/src/main/java/com/shockp/numberguessinggame/domain/model/GameState.java
+++ number-guessing-game/src/main/java/com/shockp/numberguessinggame/domain/model/GameState.java
@@ -1,8 +1,52 @@
 package com.shockp.numberguessinggame.domain.model;
 
-public class GameState {
+/**
+ * Represents the different states that a number guessing game can be in.
+ * <p>
+ * This enum defines the four possible states of a game:
+ * </p>
+ * <ul>
+ *   <li>{@link #NOT_STARTED} - The game has been created but not yet started</li>
+ *   <li>{@link #IN_PROGRESS} - The game is currently being played</li>
+ *   <li>{@link #WON} - The player has successfully guessed the number</li>
+ *   <li>{@link #LOST} - The player has run out of attempts without guessing correctly</li>
+ * </ul>
+ * <p>
+ * This enum is used throughout the game logic to track the current state
+ * and determine appropriate actions and responses.
+ * </p>
+ */
+public enum GameState {
+    /**
+     * The game has been created but not yet started.
+     * <p>
+     * This is the initial state when a new game instance is created.
+     * </p>
+     */
     NOT_STARTED,
+    
+    /**
+     * The game is currently being played.
+     * <p>
+     * This state indicates that the game is active and the player
+     * is making attempts to guess the number.
+     * </p>
+     */
     IN_PROGRESS,
+    
+    /**
+     * The player has successfully guessed the number.
+     * <p>
+     * This state indicates that the game has ended with a win.
+     * </p>
+     */
     WON,
-    LOST;
+    
+    /**
+     * The player has run out of attempts without guessing correctly.
+     * <p>
+     * This state indicates that the game has ended with a loss.
+     * </p>
+     */
+    LOST
 }
