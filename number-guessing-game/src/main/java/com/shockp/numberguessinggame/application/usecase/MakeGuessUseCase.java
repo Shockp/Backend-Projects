a129b36fc93b11d22c0abb90475986a1c3b0bb89
Shockp <masque2204@gmail.com>
2025-07-06 17:27:42 +0200

    Update to-do

--- number-guessing-game/src/main/java/com/shockp/numberguessinggame/application/usecase/MakeGuessUseCase.java
+++ number-guessing-game/src/main/java/com/shockp/numberguessinggame/application/usecase/MakeGuessUseCase.java
@@ -1,4 +1,13 @@
 package com.shockp.numberguessinggame.application.usecase;
 
+import com.shockp.numberguessinggame.application.port.UserInterface;
+import com.shockp.numberguessinggame.domain.service.GameService;
+
 public class MakeGuessUseCase {
+    
+    private final GameService gameService;
+    UserInterface userInterface;
+
+
+    
 }
