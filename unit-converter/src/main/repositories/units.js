0c3ceb958dbbf5cc4999edcd0f81e2e2c9923845
Shockp <masque2204@gmail.com>
2025-07-17 20:25:17 +0200

    units.js implemented

--- unit-converter/src/main/repositories/units.js
+++ unit-converter/src/main/repositories/units.js
@@ -1,5 +1,25 @@
 class Units {
-    // TODO: Implement Units class
+    static CATEGORIES = Object.freeze({
+       LENGTH: 'length',
+        WEIGHT: 'weight',
+        TEMPERATURE: 'temperature'
+    });
+
+    static LISTS = Object.freeze({
+        [Units.CATEGORIES.LENGTH]: [
+            'mm', 'cm', 'm', 'km',
+            'in', 'ft', 'yd', 'mi'
+        ],
+
+        [Units.CATEGORIES.WEIGHT]: [
+            'mg', 'g', 'kg', 't',
+            'oz', 'lb', 'st', 'ton'
+        ],
+
+        [Units.CATEGORIES.TEMPERATURE]: [
+            'c', 'f', 'k'
+        ]
+    });
 }
 
 module.exports = Units;
