9cc0098636886be4c8e7133eb1b51d6571212655
Shockp <masque2204@gmail.com>
2025-08-02 14:44:25 +0200

    build: remove preview features configuration from pom.xml
    The --enable-preview flags were removed from both maven-compiler-plugin and maven-surefire-plugin as they are no longer needed for Java 21. Also fixed the CommandLineRunner import path in PersonalBlogApplication.

--- personal-blog/src/main/java/com/personalblog/PersonalBlogApplication.java
+++ personal-blog/src/main/java/com/personalblog/PersonalBlogApplication.java
@@ -1,6 +1,6 @@
 package com.personalblog;
 
-import org.springframework.CommandLineRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
